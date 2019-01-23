295617547786304f1a81c10491663f3719fc2948
zhenxuanfeng <zhenxuanfeng0@gmail.com>
2019-01-23 15:47:35 +0800

    version1.1, click to export json

--- url_recorder/js/background.js
+++ url_recorder/js/background.js
@@ -1,10 +1,16 @@
-var urlList = [];
+var records = [];
 var count = 0
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if(changeInfo.url) {
-        urlList.push(tab.url);
-        var timestamp=new Date().getTime();
-        urlList.push(timestamp)
+        var record = {};
+        record['url'] = tab.url;
+        record['time'] = new Date().getTime();
+        records.push(record);
+        data = JSON.stringify(records)
+        alert(records.length);
+        // urlList.push(tab.url);
+        // var timestamp=new Date().getTime();
+        // urlList.push(timestamp)
         // chrome.storage.sync.set({'urlList': urlList}, function() {
         //     // callback body
         //     alert(urlList)
@@ -17,3 +23,13 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 
+
+// {"records":{
+//     {"url":"www.baidu.com", "time":"123567890"}
+// }}
+
+// {"sites":[
+//     {"name":"Runoob", "url":"www.runoob.com"}, 
+//     {"name":"Google", "url":"www.google.com"},
+//     {"name":"Taobao", "url":"www.taobao.com"}
+// ]}
